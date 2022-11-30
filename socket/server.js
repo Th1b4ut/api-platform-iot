91b79f9a169dbd069dc5e50dad9ec02bbff9640b
Th1b4ut <thibaut.hamelin45@gmail.com>
2022-11-30 11:41:02 +0100

    Update server.js

--- socket/server.js
+++ socket/server.js
@@ -1,7 +1,7 @@
 var SerialPort = require('serialport');
 var xbee_api = require('xbee-api');
 var C = xbee_api.constants;
-var storage = require("./storage")
+//var storage = require("./storage")
 require('dotenv').config()
 
 
@@ -12,7 +12,7 @@ var xbeeAPI = new xbee_api.XBeeAPI({
 });
 
 let serialport = new SerialPort(SERIAL_PORT, {
-  baudRate: process.env.SERIAL_BAUDRATE || 9600,
+  baudRate: 9600,
 }, function (err) {
   if (err) {
     return console.log('Error: ', err.message)
@@ -39,6 +39,7 @@ serialport.on("open", function () {
   };
   xbeeAPI.builder.write(frame_obj);
 
+
 });
 
 // All frames parsed by the XBee will be emitted here
@@ -61,16 +62,121 @@ xbeeAPI.parser.on("data", function (frame) {
   if (C.FRAME_TYPE.NODE_IDENTIFICATION === frame.type) {
     // let dataReceived = String.fromCharCode.apply(null, frame.nodeIdentifier);
     console.log("NODE_IDENTIFICATION");
-    storage.registerSensor(frame.remote64)
+    //storage.registerSensor(frame.remote64)
 
   } else if (C.FRAME_TYPE.ZIGBEE_IO_DATA_SAMPLE_RX === frame.type) {
 
     console.log("ZIGBEE_IO_DATA_SAMPLE_RX")
-    console.log(frame.analogSamples.AD0)
-    storage.registerSample(frame.remote64,frame.analogSamples.AD0 )
+    console.log(frame.analogSamples.AD1)
 
+    let water = frame.analogSamples.AD1;
+    let dataReceived = String.fromCharCode.apply(null, frame.data);
+    // console.log(">> ZIGBEE_RECEIVE_PACKET >", dataReceived);
+
+
+    if (water >= 120 && water < 280) {
+      //console.log("l'eau est à 25%")
+
+      frame_obj = { // AT Request to be sent
+        type: C.FRAME_TYPE.AT_COMMAND,
+        command: "D0",
+        commandParameter: ["0x05"],
+      };
+      xbeeAPI.builder.write(frame_obj);
+
+      frame_obj = { // AT Request to be sent
+        type: C.FRAME_TYPE.AT_COMMAND,
+        command: "D1",
+        commandParameter: ["0x05"],
+      };
+      xbeeAPI.builder.write(frame_obj);
+
+      frame_obj = { // AT Request to be sent
+        type: C.FRAME_TYPE.AT_COMMAND,
+        command: "D2",
+        commandParameter: ["0x05"],
+      };
+      xbeeAPI.builder.write(frame_obj);
+
+      frame_obj = { // AT Request to be sent
+        type: C.FRAME_TYPE.AT_COMMAND,
+        command: "D0",
+        commandParameter: ["0x04"],
+      };
+      xbeeAPI.builder.write(frame_obj);
+
+    }
+    if (water >= 280 && water < 300) {
+      // console.log("l'eau est à 50%")
+
+      frame_obj = { // AT Request to be sent
+        type: C.FRAME_TYPE.AT_COMMAND,
+        command: "D0",
+        commandParameter: ["0x05"],
+      };
+      xbeeAPI.builder.write(frame_obj);
+
+      frame_obj = { // AT Request to be sent
+        type: C.FRAME_TYPE.AT_COMMAND,
+        command: "D1",
+        commandParameter: ["0x05"],
+      };
+      xbeeAPI.builder.write(frame_obj);
+
+      frame_obj = { // AT Request to be sent
+        type: C.FRAME_TYPE.AT_COMMAND,
+        command: "D2",
+        commandParameter: ["0x05"],
+      };
+      xbeeAPI.builder.write(frame_obj);
+
+      frame_obj = { // AT Request to be sent
+        type: C.FRAME_TYPE.AT_COMMAND,
+        command: "D1",
+        commandParameter: ["0x04"],
+      };
+      xbeeAPI.builder.write(frame_obj);
+
+    }
+    if (water >= 310) {
+      // console.log("l'eau est à 75%")
+      frame_obj = { // AT Request to be sent
+        type: C.FRAME_TYPE.AT_COMMAND,
+        command: "D0",
+        commandParameter: ["0x05"],
+      };
+      xbeeAPI.builder.write(frame_obj);
+
+      frame_obj = { // AT Request to be sent
+        type: C.FRAME_TYPE.AT_COMMAND,
+        command: "D1",
+        commandParameter: ["0x05"],
+      };
+      xbeeAPI.builder.write(frame_obj);
+
+      frame_obj = { // AT Request to be sent
+        type: C.FRAME_TYPE.AT_COMMAND,
+        command: "D2",
+        commandParameter: ["0x05"],
+      };
+      xbeeAPI.builder.write(frame_obj);
+
+      frame_obj = { // AT Request to be sent
+        type: C.FRAME_TYPE.AT_COMMAND,
+        command: "D2",
+        commandParameter: ["0x04"],
+      };
+      xbeeAPI.builder.write(frame_obj);
+    }
+
+    //console.log(frame.analogSamples.AD0)
+    // storage.registerSample(frame.remote64,frame.analogSamples.AD0 )
+    // let dataReceived = String.fromCharCode.apply(null, frame.data);
+    //console.log(">> ZIGBEE_RECEIVE_PACKET >", dataReceived);
   } else if (C.FRAME_TYPE.REMOTE_COMMAND_RESPONSE === frame.type) {
     console.log("REMOTE_COMMAND_RESPONSE")
+
+
   } else {
     console.debug(frame);
     let dataReceived = String.fromCharCode.apply(null, frame.commandData)
